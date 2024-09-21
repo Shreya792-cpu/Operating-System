41b93217b8f44d59ecbb75ffbbe3f51ea169f24f
Shreya792-cpu <shreyar.23.beai@acharya.ac.in>
2024-09-21 20:06:25 +0530

    Add files via upload

---
+++ execvp2.c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+int main()
+{
+printf("Shreya Verma\n 1AY23AI109");
+char *cmnd="ls";
+char *arg_list[]={"ls","-l",NULL};
+printf("Before execvp()\n");
+printf("Creating another process using fork()\n");
+pid_t p=fork();
+int staus=0;
+if(p==0){
+printf("Child Process\n");
+int status=execvp(cmnd,arg_list);
+if(status==-1){
+printf("incorrect termination\n");
+exit(1);
+}
+}
+else{
+printf("Parent process\n");
+wait(NULL);
+printf("now this line will be executed\n");
+}
+}
+
